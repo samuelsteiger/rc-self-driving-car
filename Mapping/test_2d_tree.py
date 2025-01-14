db13daac3502441b98b27ed16d5619499c818761
Samuel Steiger <samcsteiger@gmail.com>
2025-01-14 12:56:35 -0500

    Created a function to determine if given tree is valid

--- Mapping/test_2d_tree.py
+++ Mapping/test_2d_tree.py
@@ -7,6 +7,17 @@ class TestTwoDTree(unittest.TestCase):
     def setUp(self):
         pass
 
+    def test_valid_tree(self):
+        tree1 = TwoDTree()
+        tree1.root = Node(2,4)
+        tree1.root.greater = Node(3,-1)
+        tree1.root.lesser = Node(-8,0)
+        tree1.root.greater.greater = Node(5,1)
+        tree1.root.greater.lesser = Node(7,-3)
+        tree1.root.lesser.greater = Node(-2, 9)
+        tree1.root.lesser.lesser = Node(-5,-7)
+
+        self.assertTrue(is_valid_tree(tree1))
 
     def test_insert(self):
         nodes = [
@@ -35,12 +46,44 @@ class TestTwoDTree(unittest.TestCase):
         self.assertEqual(nodes[4], tree.root.lesser.greater)
         self.assertEqual(nodes[5], tree.root.greater.lesser)
         self.assertEqual(nodes[6], tree.root.lesser.lesser)
+        self.assertTrue(is_valid_tree(tree))
 
     def test_exsits(self):
         pass
 
     def test_remove(self):
         pass
+
+def is_valid_tree(tree):
+    queue = []
+    queue.append(tree.root)
+    depth = 0
+    level_count = 1
+    while len(queue) > 0:
+        current_node = queue.pop(0)
+        if current_node.greater:
+            queue.append(current_node.greater)
+            if depth % 2 == 0:
+                if current_node.point.x >= current_node.greater.point.x:
+                    return False
+            else:
+                if current_node.point.y >= current_node.greater.point.y:
+                    return False
+        level_count+=1
+        if current_node.lesser:
+            queue.append(current_node.lesser)
+            if depth % 2 == 0:
+                if current_node.point.x < current_node.lesser.point.x:
+                    return False
+            else:
+                if current_node.point.y < current_node.lesser.point.y:
+                    return False
+        level_count+=1
+        if 2**(depth+1) < level_count:
+            depth+=1
+            level_count = 1
+    
+    return True
 
 if __name__ == "__main__":
     unittest.main()
